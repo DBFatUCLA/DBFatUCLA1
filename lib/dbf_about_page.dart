69b9baf0df9394c76ed6ec48c55d9fc1b8970485
DBFatUCLA <72041438+DBFatUCLA@users.noreply.github.com>
2020-09-29 09:23:40 -0700

    Update dbf_about_page.dart

--- lib/dbf_about_page.dart
+++ lib/dbf_about_page.dart
@@ -46,7 +46,9 @@ class DbfAboutPage extends StatelessWidget
             child: ListView(
                 padding: EdgeInsets.fromLTRB(0, 15.0, 0, 15.0),
                 children: <Widget> [
-                    _compose_text('About Us', 'About blurb goes here...'),
+                    _compose_text('About Us', 'Design Build Fly (DBF) at UCLA is an engineering club dedicated to researching, designing, manufacturing, and flying remote-controlled airplanes. Each year, club members bring together their collective knowledge and abilities to construct a plane to fit specifications outlined by the American Institute of Aeronautics and Astronautics (AIAA) for the Design Build Fly competition each spring. In addition to promoting real-world application of engineering knowledge, DBF at UCLA hosts career planning, social, and academic development events throughout the year in an effort to help students navigate the college experience and prepare for the world beyond.
+
+                    Through the annual design process and other projects, DBF members learn new skills and apply them towards real-world problem-solving. Students can pursue their interests in a variety fields, including aerodynamics, electronics, computer-aided design, and manufacturing. Each competition brings new missions and new design parameters, introducing new engineering challenges to consider throughout the design process. In addition to preparation for the annual trip to Tucson or Wichita, DBF at UCLA provides academic guidance, social events, and opportunities to participate in outreach.'),
                     _compose_text('Get Involved!', 'Join the Slack at ...'),
                 ],
             )
